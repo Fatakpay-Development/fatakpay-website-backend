39f80aa9dc698662bf72faed2c37a41505ff4ddb
@aniket_gurav <aniket.g@codezen.in>
2022-02-15 18:58:57 +0530

    add validation in contact_us and home_page

--- contact_us/serializers.py
+++ contact_us/serializers.py
@@ -13,3 +13,20 @@ class PostContactUsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactUs
         fields = ['full_name', 'email', 'message']
+        
+    def validate_full_name(self, value):
+        if value != "":
+            return value
+        raise serializers.ValidationError("Name Is Required")
+        
+    def validate_email(self, value):
+        if value != "":
+            return value
+        raise serializers.ValidationError("Email Is Required")
+    
+    def validate_message(self, value):
+        if value != "":
+            return value
+        raise serializers.ValidationError("Message Is Required")
+
+   
