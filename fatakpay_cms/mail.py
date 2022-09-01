59fee25ed4501bfe0cdab5caea183fd3b4ef0810
Quaint <gangaram.codezen.in>
2022-09-01 11:18:35 +0530

    mail attachment send changes

--- fatakpay_cms/mail.py
+++ fatakpay_cms/mail.py
@@ -1,10 +1,12 @@
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 
-def custom_mail(Subject, Message, To, html_content, From):
+def custom_mail(Subject, Message, To, html_content, From, attach=None):
     subject, from_email, to = Subject, From, To
     text_content = Message
     html_content = html_content
     msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(html_content, "text/html")
+    if (attach) :
+        msg.attach(attach.name, attach.read(), attach.content_type)
     msg.send()
