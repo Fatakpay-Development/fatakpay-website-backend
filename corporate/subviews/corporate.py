0f64967764b822ae3d377fe27afdaccc592fff6a
Quaint <gangaram.codezen.in>
2022-09-15 12:57:30 +0530

    error message changed

--- corporate/subviews/corporate.py
+++ corporate/subviews/corporate.py
@@ -17,7 +17,7 @@ class CorporateAPIView(APIView):
                 return Response({
                     'success': False,
                     'status_code': status.HTTP_400_BAD_REQUEST,
-                    'message': email_id + ' is already exists.',
+                    'message': email_id + ' is already registered with us.',
                     'data': serializer.errors},
                     status = status.HTTP_400_BAD_REQUEST)
             else:
